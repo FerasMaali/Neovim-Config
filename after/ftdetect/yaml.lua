e8af01e993c4b19ffc8fd5c944a6ab82979b301b
Feras Maali <feras@quizplus.com>
2023-12-18 20:42:57 +0200

    .kube/config file is a YAML file

---
+++ after/ftdetect/yaml.lua
@@ -0,0 +1,10 @@
+local yaml_ftdetect = vim.api.nvim_create_augroup("yaml_ftdetect", {
+    clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    desc = "YAML",
+    pattern = "*/.kube/config",
+    command = "set ft=yaml",
+    group = yaml_ftdetect,
+})
