b1b382dfb1a7fb94a7d84b366226f0aca42053d6
Feras Maali <feras@quizplus.com>
2023-12-18 20:47:22 +0200

    Don't use sleuth for YAML

--- lua/plugins/tpope.lua
+++ lua/plugins/tpope.lua
@@ -2,5 +2,22 @@ return {
     "tpope/vim-eunuch",
     "tpope/vim-surround",
     "tpope/vim-repeat",
-    "tpope/vim-sleuth",
+    {
+        "tpope/vim-sleuth",
+        init = function()
+            local my_sleuth = vim.api.nvim_create_augroup("my_sleuth", {
+                clear = true,
+            })
+
+            vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+                desc = "Turn off sleuth for YAML files",
+                pattern = {
+                    "*/.kube/config",
+                    "*.yaml",
+                },
+                command = "let b:sleuth_automatic = 0",
+                group = my_sleuth,
+            })
+        end,
+    },
 }
