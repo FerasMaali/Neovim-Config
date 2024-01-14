6b23960428009588cb72fa7c42f8378d0d749f71
Feras Maali <feras@quizplus.com>
2024-01-14 13:55:50 +0200

    feat: add a keymap to enable dropbar for the current buffer
    This should be changed to toggle. But it seems that this is currently
    not possible due to limitations from the plugin itself

--- lua/plugins/dropbar.lua
+++ lua/plugins/dropbar.lua
@@ -7,6 +7,10 @@ return {
                 general = {
                     ---@type boolean|fun(buf: integer, win: integer, info: table?): boolean
                     enable = function(buf, win, _)
+                        if vim.b.dropbar_enabled then
+                            return true
+                        end
+
                         if vim.api.nvim_win_get_config(win).zindex then
                             return false
                         end
@@ -26,6 +30,16 @@ return {
                         return allowedType[filetype]
                     end,
                 },
+            })
+
+            vim.keymap.set("n", "<leader>ub", function()
+                vim.b.dropbar_enabled = true
+                dropbar.setup()
+            end, {
+                noremap = true,
+                silent = true,
+                buffer = true,
+                desc = "Enable dropbar for the current buffer",
             })
         end,
     },
