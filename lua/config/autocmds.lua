351935a1ee052af43e4cc89a56f38ce665f13894
Feras Maali <feras@quizplus.com>
2024-01-16 14:36:16 +0200

    Persistent folds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,14 +2,17 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-local runnable = vim.api.nvim_create_augroup("runnable", {
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+local runnable = augroup("runnable", {
     clear = true,
 })
 
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "FileChangedShell" }, {
+autocmd({ "BufRead", "BufNewFile", "FileChangedShell" }, {
     desc = "Run the file using <F5> only if the file is executable",
     callback = function()
-        local filepath = vim.fn.expand('%:p')
+        local filepath = vim.fn.expand("%:p")
         if vim.fn.executable(filepath) == 1 then
             vim.keymap.set("n", "<F5>", "<CMD>term " .. filepath .. "<CR>", {
                 noremap = true,
@@ -18,4 +21,25 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "FileChangedShell" }, {
         end
     end,
     group = runnable,
+})
+
+-- Persistent Folds
+local save_fold = augroup("PersistentFolds", { clear = true })
+autocmd("BufWinLeave", {
+    callback = function()
+        if vim.fn.expand("%:p") == "" then
+            return
+        end
+        vim.cmd.mkview()
+    end,
+    group = save_fold,
+})
+autocmd("BufWinEnter", {
+    callback = function()
+        if vim.fn.expand("%:p") == "" then
+            return
+        end
+        vim.cmd.loadview({ mods = { emsg_silent = true } })
+    end,
+    group = save_fold,
 })
