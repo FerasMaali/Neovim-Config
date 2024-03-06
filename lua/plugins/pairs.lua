1b0a132725704cf137a55d736d866715a5ac9f2e
Feras Maali <feras@quizplus.com>
2024-03-06 23:38:42 +0200

    Stop flying mode

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -4,7 +4,7 @@ return {
         init = function()
             vim.g.AutoPairsMultilineClose = 0
             vim.g.AutoPairsMapCh = 0
-            vim.g.AutoPairsFlyMode = 1
+            vim.g.AutoPairsFlyMode = 0
             vim.g.AutoPairsShortcutBackInsert = "<M-b>"
             vim.g.AutoPairsShortcutToggle = "<M-p>"
             vim.g.AutoPairsShortcutFastWrap = false
