fc07f60ab313916ae3104de7b78503a05dc4f443
FerasMaali <90563363+FerasMaali@users.noreply.github.com>
2024-06-08 16:02:45 +0300

    Use Prettier for CSS

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,6 +2,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
+            ["css"] = { { "prettierd", "prettier" } },
             ["html"] = { { "prettierd", "prettier" } },
             ["markdown"] = { { "prettierd", "prettier" } },
             ["markdown.mdx"] = { { "prettierd", "prettier" } },
