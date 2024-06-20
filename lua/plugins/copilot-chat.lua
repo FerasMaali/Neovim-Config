f0d31c4279970164e799a8bd2b8046f9d6cfcefd
Feras Maali <feras@quizplus.com>
2024-06-20 18:02:32 +0300

    add prompts for copilot chat

---
+++ lua/plugins/copilot-chat.lua
@@ -0,0 +1,15 @@
+return {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+        prompts = {
+            -- Code related prompts
+            Refactor = "Please refactor the following code to improve its clarity and readability.",
+            Documentation = "Please provide documentation for the following code.",
+            -- Text related prompts
+            TextSummarize = "Please summarize the following text.",
+            TextSpelling = "Please correct any grammar and spelling errors in the following text.",
+            TextWording = "Please improve the grammar and wording of the following text.",
+            TextConcise = "Please rewrite the following text to make it more concise.",
+        },
+    },
+}
