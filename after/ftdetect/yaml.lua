c12b99d510a7656f3e129edcfc91732cd10ed660
Feras Maali <feras@quizplus.com>
2024-07-09 15:39:02 +0300

    feat(yaml): add kubeconfig to yaml file detection
    The pattern for detecting YAML files has been updated to include
    "kubeconfig" files. This change will allow Neovim to correctly
    identify and handle these files as YAML.

--- after/ftdetect/yaml.lua
+++ after/ftdetect/yaml.lua
@@ -4,7 +4,7 @@ local yaml_ftdetect = vim.api.nvim_create_augroup("yaml_ftdetect", {
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     desc = "YAML",
-    pattern = "*/.kube/config",
+    pattern = {"*/.kube/config", "kubeconfig"},
     command = "set ft=yaml",
     group = yaml_ftdetect,
 })
