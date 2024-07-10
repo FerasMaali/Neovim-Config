b1c760934738d79c1e3607ef1d9e91a7098d9d1e
FerasMaali <90563363+FerasMaali@users.noreply.github.com>
2024-07-10 07:43:33 +0300

    Formatting lines in Latex

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,19 @@
+local config = vim.fn.expand("$HOME/.indentconfig.yaml")
 return {
     "stevearc/conform.nvim",
     opts = {
+        formatters = {
+            injected = { options = { ignore_errors = true } },
+            latexindent = {
+                args = { "-m", "-l", config, "-" },
+            },
+        },
         formatters_by_ft = {
             ["css"] = { { "prettierd", "prettier" } },
             ["html"] = { { "prettierd", "prettier" } },
             ["markdown"] = { { "prettierd", "prettier" } },
             ["markdown.mdx"] = { { "prettierd", "prettier" } },
+            ["tex"] = { "latexindent" },
         },
     },
 }
